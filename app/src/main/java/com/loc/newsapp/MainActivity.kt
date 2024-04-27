74bff0d450a2a3f6ddb6566589a389dfa2704212
Guilherme Schneidt <guilhermepsch@gmail.com>
2024-04-27 18:49:07 -0300

    feat: retrofit baseline

--- app/src/main/java/com/loc/newsapp/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/MainActivity.kt
@@ -1,30 +1,26 @@
 package com.loc.newsapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.lifecycleScope
-import com.loc.newsapp.domain.usecases.AppEntryUseCases
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.loc.newsapp.presentation.navgraph.NavGraph
-import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
-import com.loc.newsapp.presentation.onboarding.OnBoardingViewModel
 import com.loc.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    val viewModel by viewModels<MainViewModel>()
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -35,6 +31,17 @@ class MainActivity : ComponentActivity() {
         }
         setContent {
             NewsAppTheme {
+
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemController = rememberSystemUiController()
+
+                SideEffect {
+                    systemController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
                     val startDestination = viewModel.startDestination
                     NavGraph(startDestination = startDestination)
