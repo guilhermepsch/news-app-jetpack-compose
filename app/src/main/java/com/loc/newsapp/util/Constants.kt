751e56b21def6a3f5a1eb22ebe4ac452a3be91f2
Guilherme Schneidt <guilhermepsch@gmail.com>
2024-04-28 16:38:03 -0300

    feat: room

--- app/src/main/java/com/loc/newsapp/util/Constants.kt
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val APP_ENTRY = "appEntry"
     const val API_KEY = "bcc3bd58dfb44ab194a055e5785c5b55"
     const val BASE_URL = "https://newsapi.org/v2/"
+    const val NEWS_DATABASE_NAME = "news_db"
 }
