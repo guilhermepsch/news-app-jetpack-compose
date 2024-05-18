ce791a2a5bd99a4ef3492eb2a7652291ed945862
Guilherme Schneidt <guilhermepsch@gmail.com>
2024-05-18 13:08:24 -0300

    feat: bookmark

--- app/src/main/java/com/loc/newsapp/presentation/bookmark/BookmarkScreen.kt
+++ app/src/main/java/com/loc/newsapp/presentation/bookmark/BookmarkScreen.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import com.loc.newsapp.R
@@ -30,11 +32,10 @@ fun BookmarkScreen(
             .padding(top = MediumPadding1, start = MediumPadding1, end = MediumPadding1)
     ) {
         Text(
-            text = "Bookmark",
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = "Favoritos",
             style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.Bold),
-            color = colorResource(
-                id = R.color.text_title
-            )
+            color = Color.White
         )
         Spacer(modifier = Modifier.height(MediumPadding1))
         ArticlesList(articles = state.articles, onClick = { navigateToDetails(it) })
